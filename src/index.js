2f6c39fb05d37315f7160b9cb61ef422e1a87f86
Donovan <divdonis@gmail.com>
2021-05-26 17:35:39 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+const grabForm = document.querySelector("#create-task-form")
+const list = document.querySelector("#tasks")
+
+grabForm.addEventListener("submit", function(y){
+  y.preventDefault()
+
+  const newInput = document.querySelector("#new-task-description").value 
+
+  const listItem = document.createElement("li")
+  listItem.innerText =  newInput
+
+  list.appendChild(listItem)
+  grabForm.reset()
+})
 });
